eacb84a148027961a58c7f66f1cc06bc19723ac5
31xSaif <81972261+ssikandersaif@users.noreply.github.com>
2026-02-13 04:08:23 +0530

    Delete web_vuln_scanner directory

--- web_vuln_scanner/analyzer/response_analyzer.py
+++
@@ -1,9 +0,0 @@
-def compare_responses(base, test):
-    score = 0
-
-    if base.status_code != test.status_code:
-        score += 1
-    if abs(len(base.text) - len(test.text)) > 50:
-        score += 1
-
-    return score >= 1
